3e98a6aba43b45d25020ff0daf9c53c98af32f07
LienPham-Automation <flow.forever.grow@gmail.com>
2026-08-31 13:59:50 +1000

    SleepLog v.7
    Bug fix on Edit button

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Sleep Log service worker: caches the app shell so it works offline after first load. */
-var CACHE = "sleeplog-v6";
+var CACHE = "sleeplog-v7";
 var ASSETS = [
   "./",
   "./index.html",
